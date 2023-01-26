075ed83238ae6d0380b7a42cda56e070dc2da658
jhunel <jcanedo0628@gmail.com>
2023-01-27 07:36:49 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const Name = "Jhunel";
+const Age = 27;
+const Rating = 5.0;
+const isComplete = true;
+const Val = null;
+const unknown = undefined;
+
+console.log(typeof Name);
+console.log(typeof Age);
+console.log(typeof Rating);
+console.log(isComplete);
+console.log(Val);
+console.log(unknown);
+
+console.log(Name);
